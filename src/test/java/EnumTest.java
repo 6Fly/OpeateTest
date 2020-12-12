83f5aa48121acc8882526cce5870169fa7aa1efa
6Fly <Ljunjie258@163.com>
2020-12-12 11:00:33 +0800

    Update EnumTest.java

--- src/test/java/EnumTest.java
+++ src/test/java/EnumTest.java
@@ -10,9 +10,6 @@ public class EnumTest {
         System.out.println(code);
         List<String> businessApply = APFile.getFile("businessApply");
         System.out.println(businessApply);*/
-        /*String str = "https://carcredit-ample.oss-cn-beijing.aliyuncs.com/BQAP%28AH%29TJ202011190003.pdf";
-        String s = str.split("\\?Expires")[0];
-        System.out.println(s);*/
         /*List list = new ArrayList<>();
         list.add(11);
         list.add(22);

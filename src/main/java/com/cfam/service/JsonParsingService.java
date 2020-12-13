a80ce8ca2a0788f3a0cccc5b15b7749a91e938c3
6Fly <Ljunjie258@163.com>
2020-12-13 16:53:03 +0800

    Update JsonParsingService.java

--- src/main/java/com/cfam/service/JsonParsingService.java
+++ src/main/java/com/cfam/service/JsonParsingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class JsonParsingService {
     public JSONArray parsJson(){
-        String json = "{\"code\":\"0\",\"msg\":\"成功\",\"data\":[{\"BQAP(AH)TJ202011090001.pdf\":{\"thumbnail\":\"http://carcredit-ample.oss-cn-beijing.aliyuncs.com/BQAP%28AH%29TJ202011090001.pdf?Expires=1604915037&OSSAccessKeyId=LTAI4FoV6VEzH2XPDcwTrceC&Signature=3zitNeBr5gxBVbDOdjkn6%2BiuCP0%3D\",\"url\":\"http://carcredit-ample.oss-cn-beijing.aliyuncs.com/BQAP%28AH%29TJ202011090001.pdf?Expires=1604915037&OSSAccessKeyId=LTAI4FoV6VEzH2XPDcwTrceC&Signature=3zitNeBr5gxBVbDOdjkn6%2BiuCP0%3D\"}}]}";
+        String json = "";
         JSONObject jsonObject = JSONObject.fromObject(json);
         JSONArray data = jsonObject.getJSONArray("data");
         return data;

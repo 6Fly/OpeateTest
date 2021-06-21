e98e25cde1e8b82fa86f405e20af20beae9197a1
liujunjie <liujunjie@carcredit.com.cn>
2021-06-21 13:49:50 +0800

    修改路径名

--- src/test/java/DownLoadFIleTEst.java
+++ src/test/java/DownLoadFIleTEst.java
@@ -2,7 +2,7 @@ import com.project.pratice.Utills.FileUploadItemunzip;
 
 public class DownLoadFIleTEst {
     public static void main(String[] args) {
-        String downloadUrl = "http://dfs.baofoo.com/group1/M00/00/58/wKi-H19Pb32AO7JlAACmzAQu82o788.zip?token=91cacfdff8f5bb9e3d60ec3025880a2d&ts=159904140";
+        String downloadUrl = "http://dfs.baofoo.com";
         String savePaths = "D:/usr/local/123.pdf";
         FileUploadItemunzip.downLoadFileFromUrlSSLAndUnzip(downloadUrl,savePaths);
     }
